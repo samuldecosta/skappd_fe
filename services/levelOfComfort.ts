2d2befc03a7ccedaea2e8081867e41b692417811
sspanchal <sspanchal@tekion.com>
2022-11-17 21:05:17 +0530

    added api integration for levelofcomfort screen

---
+++ services/levelOfComfort.ts
@@ -0,0 +1,53 @@
+//api here is an axios instance which has the baseURL set according to the env.
+import { useState } from "react";
+import api from "services/api";
+
+export const useLevelOfComfort = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [comfortSettingOptions, setComfortSettingOptions] = useState();
+  const [patientPopulationOptions, setPatientPopulationOptions] = useState();
+  const getLevelOfComfortFormPreferences = async (preference_name: string) => {
+    try {
+      const { data } = await api.get(
+        `v1/preferences?preference_name=${preference_name}`
+      );
+      return data;
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  };
+  const getComfortSettingOptions = async () => {
+    setIsLoading(true);
+    const response = await getLevelOfComfortFormPreferences(
+      "comfort_setting_preference"
+    );
+    setComfortSettingOptions(genrateformFieldOptions(response));
+    setIsLoading(false);
+  };
+
+  const getPatientPopulationOptions = async () => {
+    setIsLoading(true);
+    const response = await getLevelOfComfortFormPreferences(
+      "patient_populations_preference"
+    );
+    setPatientPopulationOptions(genrateformFieldOptions(response));
+    setIsLoading(false);
+  };
+  return {
+    getComfortSettingOptions,
+    getPatientPopulationOptions,
+    patientPopulationOptions,
+    comfortSettingOptions,
+    isLoading,
+  };
+};
+
+// helpers
+
+const genrateformFieldOptions = (apiResponse: any) => {
+  const { response } = apiResponse;
+  return response.map(({ title, id }: { title: string; id: string }) => ({
+    value: id,
+    label: title,
+  }));
+};
--- src/components/organisms/LevelOfComfort.tsx
+++ src/components/organisms/LevelOfComfort.tsx
@@ -5,10 +5,12 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { StepAccordion } from "@components/molecules/StepAccordion";
 import { FormCheckboxGrid } from "@components/atoms/FormCheckboxGrid";
 import { LevelOfComfortSchema } from "src/schema/onboardingSchema";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FormFooter } from "@components/atoms/FormFooter";
 import { set_step_completed } from "@state/onboarding";
 import { useAtom } from "jotai";
+import { useLevelOfComfort } from "services/levelOfComfort";
+import { Loader } from "@components/atoms/Loader";
 
 export const LevelOfComfort = ({
   showFooter = true,
@@ -19,6 +21,19 @@ export const LevelOfComfort = ({
   const formInstance = useForm({
     resolver: yupResolver(LevelOfComfortSchema),
   });
+
+  const {
+    getComfortSettingOptions,
+    getPatientPopulationOptions,
+    patientPopulationOptions = [],
+    comfortSettingOptions = [],
+    isLoading,
+  } = useLevelOfComfort();
+
+  useEffect(() => {
+    getComfortSettingOptions();
+    getPatientPopulationOptions();
+  }, []);
 
   const {
     control,
@@ -34,105 +49,81 @@ export const LevelOfComfort = ({
   };
 
   return (
-    <Box>
-      <Box sx={{ mx: 3, mt: 3 }}>
-        <Typography variant="h6">Level of Comfort</Typography>
-        <Typography
-          sx={{
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          {/* <AccessTimeIcon fontSize="small" /> 1-2 mins */}
-        </Typography>
-      </Box>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container sx={{ mt: 4, mb: 8 }}>
-          <Grid item xs={12} sx={{ borderBottom: "1px solid #CEE0DB" }}>
-            <StepAccordion
-              title="Comfort Setting"
-              value={getValues("comfortSetting")}
-              name="comfortSetting"
-              expanded={expanded}
-              handleChange={setExpanded}
-              isError={errors["comfortSetting"]}
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Box>
+          <Box sx={{ mx: 3, mt: 3 }}>
+            <Typography variant="h6">Level of Comfort</Typography>
+            <Typography
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
             >
-              <FormCheckboxGrid
-                field={{
-                  name: "comfortSetting",
-                  label: "Comfort Setting",
-                  control: control,
-                  options: {
-                    options: [
-                      {
-                        value: "Acute Care",
-                        label: "Acute Care",
+              {/* <AccessTimeIcon fontSize="small" /> 1-2 mins */}
+            </Typography>
+          </Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container sx={{ mt: 4, mb: 8 }}>
+              <Grid item xs={12} sx={{ borderBottom: "1px solid #CEE0DB" }}>
+                <StepAccordion
+                  title="Comfort Setting"
+                  value={getValues("comfortSetting")}
+                  name="comfortSetting"
+                  expanded={expanded}
+                  handleChange={setExpanded}
+                  isError={errors["comfortSetting"]}
+                >
+                  <FormCheckboxGrid
+                    field={{
+                      name: "comfortSetting",
+                      label: "Comfort Setting",
+                      control: control,
+                      options: {
+                        options: comfortSettingOptions,
                       },
-                      {
-                        value: "Long Term Acute Care (LTAC)",
-                        label: "Long Term Acute Care (LTAC)",
+                    }}
+                    formInstance={formInstance}
+                  />
+                </StepAccordion>
+              </Grid>
+              <Grid item xs={12} sx={{ borderBottom: "1px solid #CEE0DB" }}>
+                <StepAccordion
+                  title="Patient Population"
+                  value={getValues("patientPopulation")}
+                  name="patientPopulation"
+                  expanded={expanded}
+                  handleChange={setExpanded}
+                  isError={errors["patientPopulation"]}
+                >
+                  <Typography sx={{ mb: 2 }}></Typography>
+                  <FormCheckboxGrid
+                    field={{
+                      name: "patientPopulation",
+                      label: "Patient Population",
+                      control: control,
+                      options: {
+                        options: patientPopulationOptions,
                       },
-                      {
-                        value: "Bachelors in Health Science",
-                        label: "Bachelors in Health Science",
-                      },
-                    ],
-                  },
-                }}
-                formInstance={formInstance}
-              />
-            </StepAccordion>
-          </Grid>
-          <Grid item xs={12} sx={{ borderBottom: "1px solid #CEE0DB" }}>
-            <StepAccordion
-              title="Patient Population"
-              value={getValues("patientPopulation")}
-              name="patientPopulation"
-              expanded={expanded}
-              handleChange={setExpanded}
-              isError={errors["patientPopulation"]}
-            >
-              <Typography sx={{ mb: 2 }}></Typography>
-              <FormCheckboxGrid
-                field={{
-                  name: "patientPopulation",
-                  label: "Patient Population",
-                  control: control,
-                  options: {
-                    options: [
-                      {
-                        value: "Per Diem",
-                        label: "Per Diem",
-                      },
-                      {
-                        value: "Part Time",
-                        label: "Part Time",
-                      },
-                      {
-                        value: "Contract",
-                        label: "Contract",
-                      },
-                      {
-                        value: "Full Time",
-                        label: "Full Time",
-                      },
-                    ],
-                  },
-                }}
-                formInstance={formInstance}
-              />
-            </StepAccordion>
-          </Grid>
-          {/* User this button when save the record from Diolo */}
-          {!showFooter && (
-            <Grid item xs={12} sx={{ mt: 2, ml: 2 }}>
-              <Button variant="outlined">Save</Button>
+                    }}
+                    formInstance={formInstance}
+                  />
+                </StepAccordion>
+              </Grid>
+              {/* User this button when save the record from Diolo */}
+              {!showFooter && (
+                <Grid item xs={12} sx={{ mt: 2, ml: 2 }}>
+                  <Button variant="outlined">Save</Button>
+                </Grid>
+              )}
             </Grid>
-          )}
-        </Grid>
 
-        {showFooter ? <FormFooter /> : ""}
-      </form>
-    </Box>
+            {showFooter ? <FormFooter /> : ""}
+          </form>
+        </Box>
+      )}
+    </>
   );
 };
